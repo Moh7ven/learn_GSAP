6188573f96ac4ffd086abce88ccff39097491bc8
Moh7ven <Mohamedsangare0107@gmail.com>
2024-05-31 23:28:10 +0000

    gsap timeline

--- first_gsap/gsap.js
+++ first_gsap/gsap.js
@@ -1,4 +1,4 @@
-gsap.to(".circle", {
+/* gsap.to(".circle", {
   y: -100,
   backgroundColor: "blue",
   duration: 1,
@@ -7,5 +7,10 @@ gsap.to(".circle", {
   //repeat: -1, //repeats 3fois et repeat: -1= infini,
   //yoyo: true,
   stagger: { each: 0.2, repeat: -1, yoyo: true, from: "center" }, //Pour définir le temps entre chaque element, from: center pour définir depuis le centre
-});
+}); */
 
+const timeline = gsap.timeline({ repeat: -1, yoyo: true });
+
+timeline.to(".bob", { x: 100, duration: 1 });
+timeline.to(".bob", { y: -250, duration: 1 });
+timeline.to(".bob", { x: 250, duration: 1 });
